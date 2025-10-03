ec274af5eb83068d734a7638a230541fc92b028e
Ephraim67 <jonathaneuphrem@gmail.com>
2025-10-03 18:35:19 +0100

    Rvert back to previous

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Screens/Home';
 import About from './Screens/About';
 import Services from './Screens/Services';
-import { SpeedInsights } from '@vercel/speed-insights/react';
+
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/service' element={<Services/>}/>
-      <SpeedInsights />
+
 
      </Routes>
     </div>
